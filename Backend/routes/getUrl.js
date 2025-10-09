f4c38d0d17b11a0d7538ce556a21c35ae6ba7878
anvDT <devabvsng@gmail.com>
2025-10-10 04:16:03 +0530

    final-longUrl

--- Backend/routes/getUrl.js
+++ Backend/routes/getUrl.js
@@ -1,12 +1,19 @@
 const { Pointer } = require("../db/urlSchema");
 
 const getUrl = async (req, res) => {
-  const { tinyUrl } = req.params;
-  const tinyUrlExists = await Pointer.findOne({ tinyUrl });
-  if (!tinyUrlExists) {
-    return res.status(404).json({ message: "URL not found" });
-  }
+  try {
+    const { tinyUrl } = req.params;
+    if (!tinyUrl) {
+      return res.status(400).json({ message: "tinyUrl is required" });
+    }
+    const tinyUrlExists = await Pointer.findOne({ tinyUrl });
+    if (!tinyUrlExists) {
+      return res.status(404).json({ message: "URL not found" });
+    }
 
-  return res.redirect(tinyUrlExists.longUrl);
+    return res.redirect(tinyUrlExists.longUrl);
+  } catch (error) {
+    res.status(500).json({ message: error.message, name: error.name });
+  }
 };
 module.exports = { getUrl };
