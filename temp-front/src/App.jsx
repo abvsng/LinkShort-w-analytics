b232673e19a55e44d64e85fce88317d543b3848a
anvDT <devabvsng@gmail.com>
2025-10-10 14:12:25 +0530

    temp-front

--- temp-front/src/App.jsx
+++ temp-front/src/App.jsx
@@ -1,7 +1,39 @@
 ﻿import "./App.css";
-
+import React from "react";
 function App() {
-  return <></>;
+  const [tinyUrl, setTinyUrl] = React.useState("");
+  const inRef = React.useRef("");
+  const shorten = async () => {
+    try {
+      const res = await fetch("http://localhost:3000/api/shorten", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userId: "xyz",
+          url: inRef.current.value,
+        }),
+      });
+      const data = await res.json();
+      setTinyUrl(data.tinyUrl);
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+  return (
+    <>
+      <input ref={inRef} type="text" placeholder="Enter long url here..." />
+      <button
+        onClick={() => {
+          shorten();
+        }}
+      >
+        Shorten
+      </button>
+      <div>{tinyUrl === "" ? "" : "http://localhost:3000/api/" + tinyUrl}</div>
+    </>
+  );
 }
 
 export default App;
